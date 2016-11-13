5c5c1c2a45822bb461408f6644d7f320831cf64d
Sheallika Singh <ss5136@columbia.edu>
2016-11-13 01:32:52 -0500

    hw3_start

--- src/hw3.py
+++ src/hw3.py
@@ -20,40 +20,178 @@ from hw3_nn import LogisticRegression, HiddenLayer, LeNetConvPoolLayer, train_nn
 #Problem 1
 #Implement the convolutional neural network architecture depicted in HW3 problem 1
 #Reference code can be found in http://deeplearning.net/tutorial/code/convolutional_mlp.py
-def test_lenet():
+def test_lenet(learning_rate=0.1, n_epochs=300,nkerns=[32, 64], batch_size=500, verbose=True):
 
-#Problem 2.1
-#Write a function to add translations
-def translate_image():
-#Implement a convolutional neural network with the translation method for augmentation
-def test_lenet_translation():
+    rng = numpy.random.RandomState(23455)
+    
+    datasets = load_data(ds_rate=1.000001,theano_shared=False)
+
+    train_set_x, train_set_y = datasets[0]
+    valid_set_x, valid_set_y = datasets[1]
+    test_set_x, test_set_y = datasets[2]
+
+    # compute number of minibatches for training, validation and testing
+    n_train_batches = train_set_x.get_value(borrow=True).shape[0]
+    n_valid_batches = valid_set_x.get_value(borrow=True).shape[0]
+    n_test_batches = test_set_x.get_value(borrow=True).shape[0]
+    n_train_batches //= batch_size
+    n_valid_batches //= batch_size
+    n_test_batches //= batch_size
+
+    # allocate symbolic variables for the data
+    index = T.lscalar()  # index to a [mini]batch
+
+    # start-snippet-1
+    x = T.matrix('x')   # the data is presented as rasterized images
+    y = T.ivector('y')  # the labels are presented as 1D vector of
+                        # [int] labels
+
+    ######################
+    # BUILD ACTUAL MODEL #
+    ######################
+    print('... building the model')
+
+    # Reshape matrix of rasterized images of shape (batch_size, 32* 32)
+    # to a 4D tensor, compatible with our LeNetConvPoolLayer
+    # (32, 32) is the size of images.
+    layer0_input = x.reshape((batch_size, 3, 32, 32))
+
+    # Construct the first convolutional pooling layer:
+    # filtering reduces the image size to (32-3+1 , 32-3+1) = (30,30)
+    # maxpooling reduces this further to (30/2, 30/2) = (15, 15)
+    # 4D output tensor is thus of shape (batch_size, nkerns[0], 15, 15)
+    layer0 = LeNetConvPoolLayer(
+        rng,
+        input=layer0_input,
+        image_shape=(batch_size, 3, 32, 32),
+        filter_shape=(nkerns[0], 3, 3, 3),
+        poolsize=(2, 2)
+    )
+
+    # Construct the second convolutional pooling layer
+    # filtering reduces the image size to (15-3+1, 15-3+1) = (13, 13)
+    # maxpooling reduces this further to (13/2, 13/2) = (6, 6)
+    # 4D output tensor is thus of shape (batch_size, nkerns[1], 6, 6)
+    layer1 = LeNetConvPoolLayer(
+        rng,
+        input=layer0.output,
+        image_shape=(batch_size, nkerns[0], 15, 15),
+        filter_shape=(nkerns[1], nkerns[0], 3, 3),
+        poolsize=(2, 2)
+    )
+
+    # the HiddenLayer being fully-connected, it operates on 2D matrices of
+    # shape (batch_size, num_pixels) (i.e matrix of rasterized images).
+    # This will generate a matrix of shape (batch_size, nkerns[1] * 6 * 6),
+    # or (500, 64 * 6 * 6) = (500, 2304) with the default values.
+    layer2_input = layer1.output.flatten(2)
+
+    # construct a fully-connected sigmoidal layer
+    layer2 = HiddenLayer(
+        rng,
+        input=layer2_input,
+        n_in=nkerns[1] * 6 * 6,
+        n_out=4096,
+        activation=T.tanh
+    )
+    layer3 = HiddenLayer(
+        rng,
+        input=layer2.output,
+        n_in=4096,
+        n_out=512,
+        activation=T.tanh
+    )   
+    # classify the values of the fully-connected sigmoidal layer
+    layer4 = LogisticRegression(input=layer3.output, n_in=512, n_out=10)
+
+    # the cost we minimize during training is the NLL of the model
+    cost = layer4.negative_log_likelihood(y)
+
+    # create a function to compute the mistakes that are made by the model
+    test_model = theano.function(
+        [index],
+        layer4.errors(y),
+        givens={
+            x: test_set_x[index * batch_size: (index + 1) * batch_size],
+            y: test_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    validate_model = theano.function(
+        [index],
+        layer4.errors(y),
+        givens={
+            x: valid_set_x[index * batch_size: (index + 1) * batch_size],
+            y: valid_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+
+    # create a list of all model parameters to be fit by gradient descent
+    params = layer4.params + layer3.params + layer2.params + layer1.params + layer0.params
+
+    # create a list of gradients for all model parameters
+    grads = T.grad(cost, params)
+
+    # train_model is a function that updates the model parameters by
+    # SGD Since this model has many parameters, it would be tedious to
+    # manually create an update rule for each model parameter. We thus
+    # create the updates list by automatically looping over all
+    # (params[i], grads[i]) pairs.
+    updates = [
+        (param_i, param_i - learning_rate * grad_i)
+        for param_i, grad_i in zip(params, grads)
+    ]
+
+    train_model = theano.function(
+        [index],
+        cost,
+        updates=updates,
+        givens={
+            x: train_set_x[index * batch_size: (index + 1) * batch_size],
+            y: train_set_y[index * batch_size: (index + 1) * batch_size]
+        }
+    )
+    # end-snippet-1
+
+    ###############
+    # TRAIN MODEL #
+    ###############
+    print('... training')
+    train_nn(train_model, validate_model, test_model,n_train_batches,n_valid_batches, n_test_batches, n_epochs,verbose = True)
 
 
-#Problem 2.2
-#Write a function to add roatations
-def rotate_image():
-#Implement a convolutional neural network with the rotation method for augmentation
-def test_lenet_rotation():
+# #Problem 2.1
+# #Write a function to add translations
+# def translate_image():
+# #Implement a convolutional neural network with the translation method for augmentation
+# def test_lenet_translation():
 
-#Problem 2.3
-#Write a function to flip images
-def flip_image():
-#Implement a convolutional neural network with the flip method for augmentation
-def test_lenet_flip():
+
+# #Problem 2.2
+# #Write a function to add roatations
+# def rotate_image():
+# #Implement a convolutional neural network with the rotation method for augmentation
+# def test_lenet_rotation():
+
+# #Problem 2.3
+# #Write a function to flip images
+# def flip_image():
+# #Implement a convolutional neural network with the flip method for augmentation
+# def test_lenet_flip():
     
     
-#Problem 2.4
-#Write a function to add noise, it should at least provide Gaussian-distributed and uniform-distributed noise with zero mean
-def noise_injection():
-#Implement a convolutional neural network with the augmentation of injecting noise into input
-def test_lenet_inject_noise_input():
+# #Problem 2.4
+# #Write a function to add noise, it should at least provide Gaussian-distributed and uniform-distributed noise with zero mean
+# def noise_injection():
+# #Implement a convolutional neural network with the augmentation of injecting noise into input
+# def test_lenet_inject_noise_input():
     
-#Problem 3 
-#Implement a convolutional neural network to achieve at least 80% testing accuracy on CIFAR-dataset
-def MY_lenet():
+# #Problem 3 
+# #Implement a convolutional neural network to achieve at least 80% testing accuracy on CIFAR-dataset
+# def MY_lenet():
 
-#Problem4
-#Implement the convolutional neural network depicted in problem4 
-def MY_CNN():
+# #Problem4
+# #Implement the convolutional neural network depicted in problem4 
+# def MY_CNN():
 
 
